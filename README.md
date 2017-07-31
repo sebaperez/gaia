027bb0fdbc089d18fb1b255633c4ff353f5c7d7e
sebaperez <sebalanus@gmail.com>
2017-07-31 18:12:00 -0300

    Se elimina viejo modulo de usuarios

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # gaia
 mail-bot para coordinar y agendar reuniones (Proyecto Final UTN FRBA - 2017)
+
+## Inicialización
+
+### Modulo de usuarios
+- FW: Loopback
+- Puerto: 3000
+```
+node src/user/.
+```
--- user/models/user.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose'), Schema = mongoose.Schema;
-
-var userSchema = new Schema({
-	name: { type: String },
-	lastname: { type: String },
-	email: { type: String }
-});
-
-module.exports = mongoose.model('user', userSchema);
