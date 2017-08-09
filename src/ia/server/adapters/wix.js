96818b248b9ef79d59c2fe29a07216ca71c7bbb9
Nicolas Presta <nicolas.presta@mercadolibre.com>
2017-08-09 00:56:50 -0300

    Agrego avances

--- src/ia/server/adapters/wix.js
+++ src/ia/server/adapters/wix.js
@@ -9,7 +9,37 @@ var exports = module.exports = {};
 
 // Convierte la respuesta de wix al formato interno de gaia.
 function adaptar(data){
-    return data
+
+    retorno = {}
+
+    retorno.original_response = data
+
+    if (data.entities){
+        retorno.ok = true
+
+        if (data.entities.intent){
+            data.entities.intent.forEach((value, index)=>{
+                retorno.intent = value.value
+            })
+
+        } else {
+            retorno.intent = "no_identificado"
+        }
+
+        retorno.fecha_propuesta = []
+
+        if (data.entities.datetime){
+            data.entities.datetime.forEach((value, index)=>{
+                retorno.fecha_propuesta.push(value.value)
+            })
+
+        }
+
+    } else {
+        retorno.ok = false
+    }
+
+    return retorno
 }
 
 exports.process = function(msj, callback) {
