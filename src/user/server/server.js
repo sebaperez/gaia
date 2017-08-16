d415d8888d074f9bdb06b86b626d3c5182373858
jonybuzz <buzzetti.jp@gmail.com>
2017-08-15 23:01:29 -0300

    Creacion de proyecto orquestador con yeoman rest

--- src/user/server/server.js
+++ src/user/server/server.js
@@ -4,7 +4,7 @@ var loopback = require('loopback');
 var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
-
+ 
 app.start = function() {
   // start the web server
   return app.listen(function() {
