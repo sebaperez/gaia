25bde1073f17badf3d79605ac007091bf5eb6fa3
jonybuzz <buzzetti.jp@gmail.com>
2017-09-19 22:03:23 -0300

    Archivo para pullear y arrancar los modulos en el servidor

---
+++ conf/checkout.sh
@@ -0,0 +1,26 @@
+forever stopall
+
+echo ========== Checkouteando =========
+cd gaia
+
+git pull https://github.com/sebaperez/gaia.git
+#git pull -b <branch> https://github.com/sebaperez/gaia.git
+
+echo ============== Instalando ==============
+cd src
+
+cd user
+npm install
+forever start .
+cd ..
+
+cd ia
+npm install
+forever start ./server/index.js
+cd ..
+
+cd orquestador
+npm install
+forever start ./config/www
+cd ..
+echo ============== Desplegado ==============
