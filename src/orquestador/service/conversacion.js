5de8f4dde288f710d46578d86e2160c9d85369af
jonybuzz <buzzetti.jp@gmail.com>
2017-09-24 14:46:52 -0300

    Ejemplo de request a conversaciones

--- src/orquestador/service/conversacion.js
+++ src/orquestador/service/conversacion.js
@@ -5,12 +5,28 @@ function crearConversacion(ownerMail, guestMail, contenidoMailActual, significad
 
    var conversacionesUrl = config.conversacionApiUrls.conversaciones;
 
+   // ej significado: {
+   //   "original_response": "PARA DEBUG",
+   //   "ok": true,
+   //   "intents": [
+   //      "solicitar_reunion"
+   //   ],
+   //   "fechas": [{
+   //      "fecha": "2017-08-22T00:00:00.000-03:00"
+   //   }],
+   //   "intervalos": [
+   //     {
+   //        "desde": "2017-08-17T00:00:00.000-03:00",
+   //        "hasta": "2017-09-02T00:00:00.000-03:00"
+   //     }],
+   // }
+
    var nuevaConversacion = {
       owner: ownerMail,
       guests: guestMail,
       mensajes: [
          {
-            contenido: contenidoMailActual,
+            contenido: contenidoMailActual, //solo el ultimo mensaje de la cadena
             significado: significado
          }
       ]
