0315eff90d50939fc0b7b5586eddcd2a54e68a5b
jonybuzz <buzzetti.jp@gmail.com>
2017-09-24 16:13:50 -0300

    Adapto config para que tome url productivas en el servidor

--- src/orquestador/config/config.js
+++ src/orquestador/config/config.js
@@ -1,15 +1,19 @@
+var urlBaseProd = "http://" + process.env.IP;
+
+var prod = process.env.ENTORNO == 'prod'
+
 module.exports.config = {
    userApiUrls: {
-      clientes: "http://localhost:5555/test/user/api/Clientes"
+      clientes: prod ? urlBaseProd + ":3000/api/Clientes": "http://localhost:5555/test/user/api/Clientes"
    },
    iaApiUrls: {
-      procesar: "http://localhost:5555/test/ia/process"
+      procesar: prod ? urlBaseProd + ":3001/process" : "http://localhost:5555/test/ia/process"
    },
    conversacionApiUrls: {
       conversaciones: "http://localhost:5555/test/conversacion"
    },
    respuestaApiUrls: {
-      solicitarReunionAlGuest: "http://localhost:5555/test/respuesta/solicitarReunionAlGuest/parser"
+      solicitarReunionAlGuest: prod ? urlBaseProd + ":9000/respuesta/solicitarReunionAlGuest/parser" + "http://localhost:5555/test/respuesta/solicitarReunionAlGuest/parser"
    },
    calendarioApiUrls: {
       huecos: "http://localhost:5555/test/calendario/huecos"
