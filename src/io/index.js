3a3390297b68407d4b27f3267a97a74316aa2c08
root <root@gaiameet.com>
2017-09-24 20:17:08 +0000

    avances

--- src/io/index.js
+++ src/io/index.js
@@ -1,19 +1,57 @@
-// Packages Imports
-const http = require('http');
-const express = require('express');
-const bodyParser = require("body-parser");
+const SMTPServer = require('smtp-server').SMTPServer;
+const getStream = require('get-stream');
 
-// Constant Definition
-const app = express();
-const server = http.createServer(app);
-//const port = process.env.PORT || 3000
-const port = process.argv[2] || 3090;
+const server = new SMTPServer({
+        authOptional: true,
 
-// Middlewares
-app.use(bodyParser.json());
+	onData(stream, session, callback){
+		const chunks = [];
+		getStream(stream).then(str => {
+			    console.log(str);
+		});
 
-// Puntos de entrada REST
-require('./api/router')(app);
+		//stream.on("data", function (chunk) {
+		//	chunks.push(chunk);
+		//	console.log(chunk);
+	        //});
 
-// Nos ponemos a escuchar... hello!
-server.listen(port, () => console.log(`Servidor Iniciado en puerto ${port}`));
+		//console.log(msj_str)
+		//stream.pipe(process.stdout); // print message to console
+		//stream.on('end', fin => {
+		//	console.log(Buffer.concat(chunks))
+		//	callback()
+		//});
+	},
+
+	onAuth(auth, session, callback){
+		console.log("on auth")
+		callback(null, {user: 123}); // where 123 is the user id or similar property
+	},
+
+	onConnecit(session, callback){
+		console.log("on connect")
+		return callback(); // Accept the connection
+	},
+
+	onMailFrom(address, session, callback){
+        	console.log('Nuevo mail desde %s', address.address)
+        	return callback(); // Accept the address
+    	},
+
+	onClose(session){ 
+		console.log("close") 
+	},
+
+	onRcptTo(address, session, callback){
+		console.log('on rcpt to')
+		return callback();
+	}
+});
+
+server.on('error', err => {
+	    console.log('Error %s', err.message);
+});
+
+server.listen(25,"gaiameet.com",callback => { console.log("estamos ok") } );
+
+
