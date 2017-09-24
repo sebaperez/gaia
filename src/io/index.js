12e742681760542139e219295f4d0382808162df
Nicolas Presta <nicolas.presta@mercadolibre.com>
2017-09-24 19:38:27 -0300

    avances

--- src/io/index.js
+++ src/io/index.js
@@ -3,6 +3,18 @@ const getStream = require('get-stream');
 const simpleParser = require('mailparser').simpleParser;
 const request = require('request'); 
 
+// Packages Imports
+const http = require('http');
+const express = require('express');
+const bodyParser = require("body-parser");
+
+// Constant Definition
+const app = express();
+const server_out = http.createServer(app);
+//const port = process.env.PORT || 3000
+const port = process.argv[2] || 4444;
+
+
 const server = new SMTPServer({
         authOptional: true,
 
@@ -66,5 +78,16 @@ server.on('error', err => {
 });
 
 server.listen(25,"gaiameet.com",callback => { console.log("estamos ok") } );
+
+
+
+// Middlewares
+app.use(bodyParser.json());
+
+// Puntos de entrada REST
+require('./api/router')(app);
+
+// Nos ponemos a escuchar... hello!
+server_out.listen(port, () => console.log(`Servidor server_out Iniciado en puerto ${port}`));
 
 
