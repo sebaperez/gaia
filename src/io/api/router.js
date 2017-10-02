7ebca967262ad3210e826a3a6bc3f4e7ace5400e
Nicolas Presta <nicolas.presta@mercadolibre.com>
2017-10-02 00:57:45 -0300

    avances

--- src/io/api/router.js
+++ src/io/api/router.js
@@ -1,104 +1,43 @@
 const nodemailer = require("nodemailer");
-const sendmail = require('sendmail')();
-var email = require('mailer');
-var smtpTransport = require('nodemailer-smtp-transport');
-
 
 module.exports = function(app) {
 
-  app.post('/send2', (req, res) => {
-  
-	  var transporter = nodemailer.createTransport(smtpTransport({
-		     host: 'mail.gaiameet.com',
-		     port: 25,
-		     debug: true,
-		     secure: false
-	  }));
+	app.post('/send', (req, res) => {
 
-	  transporter.sendMail({
-		     from: 'clara@gaiameet.com',
-		     to: 'presta.nico@gmail.com',
-		     subject: 'hello',
-		     html: '<b>hello world!</b>',
-		     text: 'hello world!'
-	  }, (error, info) => {
-	  	 if (error) {
-			             console.log('----> Error occurred on email send');
-			             console.log(error.message);
-			    }
+		console.log("--> to " + req.body.to);
+		console.log("--> from " + req.body.from);
+		console.log("--> text " + req.body.text);
+		console.log("--> subject " + req.body.subject);
+		console.log("--> inReplyTo " + req.body.inReplyTo);
 
-		          console.log('----> Message sent successfully!');
-		          console.log(nodemailer.getTestMessageUrl(info));
+		var smtp = nodemailer.createTransport({
+						service: "Zoho",
+						auth: {
+							user: req.body.from,
+							pass: "UTNfrba1"
+						}
+					});
 
-		          // only needed when using pooled connections
-		  	   transporter.close();
+		smtp.sendMail({
+					from: req.body.from,
+					to: req.body.to,
+					subject: req.body.subject,
+					text: req.body.text,
+					inReplyTo: req.body.inReplyTo
+	 			}, function(error, info){
+					if(error){
+						console.log(error);
+					}else{
+						console.log('Message sent: ' + info.response);
+					}
+		});
 
-	  });
-  })
+		res.status(200).json({"ok":"ok"});
 
-  app.post('/send', (req, res) => {
-
-      console.log("--> to " + req.body.to);
-      console.log("--> from " + req.body.from);
-      console.log("--> text " + req.body.text);
-      console.log("--> subject " + req.body.subject);
-      console.log("--> inReplyTo " + req.body.inReplyTo);
-
-      var message = {
-          from: req.body.from,
-          to: req.body.to,
-          subject: req.body.subject,
-          text: req.body.text //,
-        //  inReplyTo: req.body.inReplyTo
-      };
-
-      var transporter = nodemailer.createTransport('smtps://clara:UTNfrba1@smtp.gmail.com');
-
-	//	transporter = nodemailer.createTransport({
-	//	    	service: 'Gmail',
-	//   		auth: {
-	//	            user: 'clara@gaiameet.com',
-	//	            pass: 'UTNfrba1'
-	//	        }
-	//		});
-		let mailOptions = {
-		             from: 'clara@gaiameet.com', // sender address
-		             to: 'presta.nico@gmail.com', // list of receivers
-		             subject: 'Hello', // Subject line
-		             text: 'Hello world?', // plain text body
-		             html: '<b>Hello world?</b>' // html body
-		         };
-
-	     transporter.sendMail(mailOptions, (error, info) => {
-		             if (error) {
-				                 return console.log(error);
-				             }
-		             console.log('Message sent: %s', info.messageId);
-		         });
-
-     res.status(200).json({"ok":"ok"});
-  });
-
-  app.get('/dummy', (req, res) => {
-
-	email.send({  
-	  	host : "gaiameet.com",              // smtp server hostname
-	  	port : "25",                     // smtp server port
-	  	domain : "gaiameet.com",            // domain used by client to identify itself to server
-	  	to : "presta.nico@gmail.com",
-	  	from : "clara@gaiameet.com",
-	  	subject : "node_mailer test email",
-	 	body: "Hello! This is a test of the node_mailer.",
-	  	authentication : "no",        // auth login is supported; anything else is no auth
-	  	username : "clara",       // Base64 encoded username
-	  	password : "clara"        // Base64 encoded password
-	},
-
-	function(err, result){  
-		  if(err){ console.log(err); }
 	});
 
-	res.status(200).json({"ok":"ok"});
-  });
+	app.get('/dummy', (req, res) => {
+		res.status(200).json({"ok":"ok"});
+	});
 
 };
--- src/io/index.js
+++ src/io/index.js
@@ -1,67 +1,16 @@
 var inbox = require("inbox");
 var nodemailer = require("nodemailer");
+const http = require('http');
+const express = require('express');
+const bodyParser = require("body-parser");
+const getStream = require('get-stream');
+const simpleParser = require('mailparser').simpleParser;
 
-var smtp = nodemailer.createTransport({
-	  service: "Zoho",
-	  auth: {
-		      user: "clara@gaiameet.com",
-		      pass: "UTNfrba1"
-		    }
-});
-  smtp.sendMail({
-	      from: "clara@gaiameet.com",
-	      to: "presta.nico@gmail.com",
-	      subject: "Hello world!",
-	      html: "Welcome!"
-	    }, function(error, info){
-		        if(error){
-				      console.log(error);
-				    }else{
-					          console.log('Message sent: ' + info.response);
-					        }
-		      });
+// Constant Definition
+const app = express();
+const server_out = http.createServer(app);
+const port = process.argv[2] || 4444;
 
-/*
-var client = inbox.createConnection(993, "imappro.zoho.com", {
-	  secureConnection: true,
-	  auth:{
-		      user: "clara@gaiameet.com",
-		      pass: "UTNfrba1"
-		    }
-});
-
-client.on("connect", function(){
-	  console.log("Successfully connected to server");
-	  
-	  client.openMailbox('INBOX', function(error, info){
-		      if(error) 
-			  console.log(error);
-		    });
-});
-
-client.on("new", function(message){
-	  console.log(message.from.address + " - " + message.title);
-
-	  smtp.sendMail({
-		      from: "clara@gaiameet.com",
-		      to: message.from.address,
-		      subject: "Hello world!",
-		      html: "Welcome!"
-		    }, function(error, info){
-			        if(error){
-					      console.log(error);
-					    }else{
-						          console.log('Message sent: ' + info.response);
-						        }
-			      });
-
-});
-
-<<<<<<< HEAD
-client.connect();
-
-*/
-=======
 // Middlewares
 app.use(bodyParser.json());
 
@@ -70,4 +19,88 @@ require('./api/router')(app);
 
 // Nos ponemos a escuchar... hello!
 server_out.listen(port, () => console.log(`Servidor server_out Iniciado en puerto ${port}`));
->>>>>>> ece12654e05cdea6100e02820df733d82e3b8442
+
+
+function get_client(mail){
+
+	var client = inbox.createConnection(993, "imap.zoho.com", {
+	  secureConnection: true,
+	  auth:{
+		      user: mail,
+		      pass: "UTNfrba1"
+		    }
+	});
+
+	client.on("connect", function(){
+		console.log("Successfully connected to server");
+
+		client.openMailbox('INBOX', function(error, info){
+			if(error) console.log(error);
+			if(info) console.log(info);
+		});
+
+	});
+
+	client.on("error", function(error){
+		console.log("EMAIL CLIENT: Ocurrio un error");
+		if(error) console.log(error);
+	});
+
+	client.on("new", function(message){
+
+		console.log(JSON.stringify(message, null, 4))
+		var messageStream = client.createMessageStream(message.UID)
+
+		getStream(messageStream).then(str => {
+				simpleParser(str, (err, mail)=>{
+					// Log por consola del mail recibido
+					console.log(JSON.stringify(mail, null, 4))
+
+					// Se lo mandamos al orquestador
+					/*
+					request.post({
+							url: "http://" + process.env.IP + ":5555/mensajes",
+							json: true,
+							body: mail
+						     },
+						function (error, responde, body){
+							console.log(JSON.stringify(body, null, 4))
+						})
+					*/
+
+					// Respondemos un dummy
+					var smtp = nodemailer.createTransport({
+						service: "Zoho",
+						auth: {
+							user: mail.to.text,
+							pass: "UTNfrba1"
+						}
+					});
+
+					smtp.sendMail({
+								from: mail.to.text,
+								to: mail.from.text,
+								subject: mail.subject,
+								text: "Por el momento no estamos online, proximamente!",
+								inReplyTo: mail.messageId
+				 			}, function(error, info){
+								if(error){
+									console.log(error);
+								}else{
+									console.log('Message sent: ' + info.response);
+								}
+					});
+				})
+			});
+
+	});
+
+	return client
+}
+
+get_client("clara@gaiameet.com").connect();
+get_client("paula@gaiameet.com").connect();
+get_client("belen@gaiameet.com").connect();
+get_client("maria@gaiameet.com").connect();
+get_client("esteban@gaiameet.com").connect();
+
