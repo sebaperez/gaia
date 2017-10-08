e0304f7a36eb6e9279fa24b3148a329c7e5a1d73
jonybuzz <buzzetti.jp@gmail.com>
2017-10-08 13:46:33 -0300

    Agrego logs para request usuario

--- src/orquestador/service/usuario.js
+++ src/orquestador/service/usuario.js
@@ -5,19 +5,25 @@ function obtenerUsuario(emailRemitente, emailDestinatario, callback, err) {
 
    var usuarioRemitenteUrl = config.userApiUrls.clientes + '?filter[where][email]=' + emailRemitente + '&filter[limit]=1';
    request.get(usuarioRemitenteUrl, function (error, response, body) {
-      var usuarioRemitente = JSON.parse(body)[0];
-      if(usuarioRemitente) {
-         callback(usuarioRemitente);
+      console.log("Buscando owner entre mails " + emailRemitente);
+      if(body){
+         var usuarioRemitente = JSON.parse(body)[0];
+         if(usuarioRemitente) {
+            callback(usuarioRemitente);
+         } else {
+            console.log("Buscando owner entre mails " + emailDestinatario);
+            var usuarioDestinatarioUrl = config.userApiUrls.clientes + '?filter[where][email]=' + emailDestinatario + '&filter[limit]=1';
+            request.get(usuarioDestinatarioUrl, function (error, response, body) {
+               var usuarioDestinatario = JSON.parse(body)[0];
+               if(usuarioDestinatario) {
+                  callback(usuarioDestinatario);
+               } else {
+                  err("No existe un owner con el email " + emailRemitente + " ni " + emailDestinatario);
+               }
+            });
+         }
       } else {
-         var usuarioDestinatarioUrl = config.userApiUrls.clientes + '?filter[where][email]=' + emailDestinatario + '&filter[limit]=1';
-         request.get(usuarioDestinatarioUrl, function (error, response, body) {
-            var usuarioDestinatario = JSON.parse(body)[0];
-            if(usuarioDestinatario) {
-               callback(usuarioDestinatario);
-            } else {
-               err("No existe un owner con el email " + emailRemitente + " ni " + emailDestinatario);
-            }
-         });
+         err("Error en el modulo de clientes")
       }
    });
 
