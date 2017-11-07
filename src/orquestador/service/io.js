381702cfef49b2a506691bc23e687db5347d9231
jonybuzz <buzzetti.jp@gmail.com>
2017-11-07 01:00:13 -0300

    Respuesta de mails estilo Gmail

--- src/orquestador/service/io.js
+++ src/orquestador/service/io.js
@@ -10,15 +10,23 @@ module.exports.responderMail = function (mailRemitente, mailDestinatario, mailCC
 
    var enviarMailUrl = config.ioApiUrls.enviar;
 
-   var headerNuevoMensaje = '<div dir="ltr">' + mensaje + '<div><br></div><div><br>'
-   var headerMailRespondido = '<div>' +
-   '---------- Mensaje enviado ----------<br>' +
-   'De: <b>'+ mail.from.value[0].name +'</b> &lt;<a href="mailto:'+ mail.from.value[0].address +'" target="_blank" rel="noopener noreferrer">'+ mail.from.value[0].address +'</a>&gt;<br>' +
-   'Fecha: ' + moment().format("d [de] MMMM [de] YYYY, HH:mm") + '<br>' +
-   'Asunto: Re: ' + mail.subject + '<br>' +
-   'Para: '+ mail.to.value[0].name +' &lt;<a href="mailto:'+ mail.to.value[0].address +'" target="_blank" rel="noopener noreferrer">'+ mail.to.value[0].address +'</a>&gt;<br>' +
-   '<br><br>'
-   var footerMailRespondido = '</div></div>'
+   // var headerNuevoMensaje = '<div dir="ltr">' + mensaje + '<div><br></div><div><br>'
+   // var headerMailRespondido = '<div>' +
+   // '---------- Mensaje enviado ----------<br>' +
+   // 'De: <b>'+ mail.from.value[0].name +'</b> &lt;<a href="mailto:'+ mail.from.value[0].address + '" target="_blank" rel="noopener noreferrer">'+ mail.from.value[0].address +'</a>&gt;<br>' +
+   // 'Fecha: ' + moment().format("d [de] MMMM [de] YYYY, HH:mm") + '<br>' +
+   // 'Asunto: Re: ' + mail.subject + '<br>' +
+   // 'Para: '+ mail.to.value[0].name +' &lt;<a href="mailto:'+ mail.to.value[0].address +'" target="_blank" rel="noopener noreferrer">'+ mail.to.value[0].address +'</a>&gt;<br>' +
+   // '<br><br>'
+   // var footerMailRespondido = '</div></div>'
+
+   // ESTILO GMAIL
+   var headerMailRespondido = '<br><br><br><div class="gmail_extra"><br>' +
+   '<div class="gmail_quote">' +
+   'El ' + moment().format("d [de] MMMM [de] YYYY, HH:mm") + ', '+ mail.from.value[0].name +'<span dir="ltr">&lt;<a href="mailto:'+ mail.from.value[0].address +'" target="_blank">'+ mail.from.value[0].address +'</a>&gt;</span> escribió:<br>' +
+   '<blockquote class="gmail_quote" style="margin:0 0 0 .8ex;border-left:1px #ccc solid;padding-left:1ex">' +
+   '<div dir="ltr"><span>'
+   var footerMailRespondido = '</span><br></div></blockquote></div><br></div>'
    var footerNuevoMensaje = '</div>'
 
    var nuevoMail = {
@@ -27,7 +35,7 @@ module.exports.responderMail = function (mailRemitente, mailDestinatario, mailCC
       cc: mailCC,
       subject: mail.subject.indexOf('Re:') == 0? 'Re: ' + mail.subject : mail.subject,
       inReplyTo: mail.messageId,
-      html: headerNuevoMensaje + headerMailRespondido + mail.html + footerMailRespondido + footerNuevoMensaje,
+      html: mensaje + headerMailRespondido + mail.html + footerMailRespondido,
       text: mensaje
    }
 
